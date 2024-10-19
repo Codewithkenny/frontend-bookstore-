6c55410d8e9b4889651aa252542ee86161d3e4f8
poju <pojuagbomeji@gmail.com>
2024-10-20 00:29:30 +0100

    working on the homeage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ], // Removed extra comma here
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FFB200',
+        'secondary': '#EB5B00',
+        'blackBG': '#F3F3F3',
+        'Accent': '#E4003A'
+      },
+      fontFamily: {
+        'primary' : ["Montserrat", "sans-serif"],
+        'secondary': ["Nunito Sans", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
