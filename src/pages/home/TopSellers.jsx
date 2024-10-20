fd9513d2a453021fb6dca2d055b2bd630a56f2f0
poju <pojuagbomeji@gmail.com>
2024-10-20 15:39:49 +0100

    added new pages

---
+++ src/pages/home/TopSellers.jsx
@@ -0,0 +1,13 @@
+import React,{ useState } from 'react'
+
+const TopSellers = () => {
+    const [books, setBooks] = useState();
+  return (
+    
+   <>
+   <h1>Topsellers</h1>
+   </>
+  )
+}
+
+export default TopSellers
